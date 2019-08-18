3fb16ffcfefff3f1dd02f633a4e5db6906196916
Aleksandr <zhuravlev.a.a@me.com>
2019-08-18 11:30:02 +0300

    Автоматизирую оптимизацию изображений в webp

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ var csso = require("gulp-csso");
 
 var imagemin = require("gulp-imagemin");
 
+var webp = require("gulp-webp");
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -35,6 +37,12 @@ gulp.task("images", function () {
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
+    .pipe(gulp.dest("source/img"));
+  });
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
   });
 
