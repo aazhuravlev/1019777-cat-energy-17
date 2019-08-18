e4efe06b946a6e101dcd595a3711bf043e8369f8
Aleksandr <zhuravlev.a.a@me.com>
2019-08-18 12:41:09 +0300

    Автоматизирую создание спрайта свг и добавления в разметку

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,11 @@ var imagemin = require("gulp-imagemin");
 
 var webp = require("gulp-webp");
 
+var svgstore = require("gulp-svgstore");
+
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -45,6 +50,23 @@ gulp.task("webp", function () {
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
   });
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sprite-icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+  });
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
+});
 
 gulp.task("server", function () {
   server.init({
